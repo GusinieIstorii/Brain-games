0ee931c71884d15d4130f33e714b57b2589677a5
Kseniia <ksenia.guseva789@gmail.com>
2023-04-18 16:34:13 +0400

    update gcd-game.js

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -3,24 +3,29 @@ import getRandomNumber from '../getRandomNumber.js';
 
 const rules = 'Find the greatest common divisor of given numbers.';
 
+const getCommonDivider = (number1, number2) => {
+  let correctAnswer;
+  let min;
+
+  if (number1 < number2) {
+    min = number1;
+  } else {
+    min = number2;
+  }
+  for (let i = 1; i <= min; i += 1) {
+    if (number1 % i === 0 && number2 % i === 0) {
+      correctAnswer = i;
+    }
+  }
+  return correctAnswer;
+};
+
 const getInputGame = () => {
   const randomNumber1 = getRandomNumber();
   const randomNumber2 = getRandomNumber();
 
   const expression = `${randomNumber1} ${randomNumber2}`;
-  let correctAnswer;
-  let min;
-
-  if (randomNumber1 < randomNumber2) {
-    min = randomNumber1;
-  } else {
-    min = randomNumber2;
-  }
-  for (let i = 1; i <= min; i += 1) {
-    if (randomNumber1 % i === 0 && randomNumber2 % i === 0) {
-      correctAnswer = i.toString();
-    }
-  }
+  const correctAnswer = getCommonDivider(randomNumber1, randomNumber2).toString();
 
   return [expression, correctAnswer];
 };
