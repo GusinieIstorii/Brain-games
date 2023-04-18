bebc1d903cdcb8c37b6f321193babf9450cadc45
Kseniia <ksenia.guseva789@gmail.com>
2023-04-18 16:39:48 +0400

    update even-game.js

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -3,14 +3,11 @@ import getRandomNumber from '../getRandomNumber.js';
 
 const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const isEven = (expression) => (expression % 2 === 0 ? 'yes' : 'no');
+
 const getInputGame = () => {
   const expression = getRandomNumber();
-  let correctAnswer;
-  if (expression % 2 === 0) {
-    correctAnswer = 'yes';
-  } else {
-    correctAnswer = 'no';
-  }
+  const correctAnswer = isEven(expression);
   return [expression, correctAnswer];
 };
 
